0293d4e0ac98955528096f8b2f8d371a832ab8a4
Oussama Krich <okrich@e3r2p3.1337.ma>
2024-02-11 14:05:12 +0100

    Auth pages responsive but still error not added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,40 @@
-import "./App.css";
+import Home from './Home/Home';
+import Login from './Auth/login/Login';
+import AuthWrapper from './Auth/AuthWrapper';
+import Register from './Auth/register/Register';
+import Welcome from './Auth/welcome/Welcome';
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/",
+    element: <AuthWrapper component={ Welcome } />,
+  },
+  {
+    path: "/login",
+    element: <AuthWrapper component={ Login } />,
+  },
+  {
+    path: "/register",
+    element: <AuthWrapper component={ Register } />,
+  },
+
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
 
 function App() {
+
   return (
-    <>
-      <h1>Transcendence</h1>
-    </>
+
+    <RouterProvider router={router} />
   );
 }
-
 export default App;
