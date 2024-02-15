addd97fd4ca5f1874a601d605e6589657267a7bd
Oussama Krich <okrich@e3r2p9.1337.ma>
2024-02-15 12:01:30 +0100

    Test 42Oauth in welcome page

--- Frontend/src/Auth/welcome/Welcome.jsx
+++ Frontend/src/Auth/welcome/Welcome.jsx
@@ -23,6 +23,65 @@ function Welcome() {
   // }, []);
 
 
+  const get42Token = async (code) =>{
+
+    let body = {code: code}
+    try{
+        const response = await fetch("http://localhost:8000/Auth/42",{
+          method: 'POST',
+          headers:{"Content-Type": "application/json"},
+          body: JSON.stringify(body),
+        });
+    
+        if (response.ok) {
+          const tokens = await response.json();
+          settoken(tokens);
+          console.log("Login successful");
+          navigate("/home");
+        } else {
+          console.error("Login failed");
+        }
+    } catch (error) {
+      console.error("Network error:", error);
+  }
+
+  
+
+  }
+
+  useEffect(()=>{
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let code = urlParams.get('code');
+
+    if (code){
+      console.log('code', code)
+      get42Token(code)  
+    }
+
+  }, [])
+
+  
+
+  const handel42 = async (e) =>{
+    e.preventDefault();
+    
+    const externalUrl = 'https://api.intra.42.fr/oauth/authorize';  
+    const params = {
+      client_id : "u-s4t2ud-ef24706709b2ebced52c2f14a643d130751366c3ebabc309cb18be033c4f8259",
+      redirect_uri : "http://localhost:8080/login",
+      response_type: 'code',
+    };
+
+      const queryString = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .join('&');
+  
+      const redirectUrl = `${externalUrl}?${queryString}`;      
+      window.location.href = redirectUrl;
+  }
+
+
   function Tologin(){
     navigate("/login");
   }
@@ -42,7 +101,7 @@ function Welcome() {
     <button className='submit-btn' onClick={Toregister}>Sign Up</button>
     <div className="other-button">
           <button className="other-method google"></button>
-          <button className="other-method intra"></button>
+          <button className="other-method intra" onClick={handel42}></button>
         </div>
         </>
     )
