2f4704dd75e6ea406e2390f0adfc88ff6ff6fb91
radouane iben-hamou <redouanebh4@gmail.com>
2024-02-20 18:59:14 +0100

    @RED-MEGA :  logout view (need test)

--- Backend/Authentication/urls.py
+++ Backend/Authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import token_refresh as jwt_token_refresh
+from rest_framework_simplejwt.views import (
+    token_refresh as jwt_token_refresh,
+    token_blacklist as jwt_token_blacklist
+)
 from .views import Register, Login, TwoFactorAuthView
 
 urlpatterns = [
@@ -7,9 +10,11 @@ urlpatterns = [
     path("register", Register.as_view(), name="register"),
     # Login
     path("login", Login.as_view(), name="login"),
+    # Logout
+    path("logout", jwt_token_blacklist, name="logout"),
+    # To Refresh the JWT token
+    path("refresh_token", jwt_token_refresh, name="token_obtain_pair"),
     # 2FA
     path("2FA", TwoFactorAuthView.as_view(), name="2FA"),
     path("2FA/qrcode", TwoFactorAuthView.get_qrcode, name="2FA_qrcode"),
-    # To Refresh the JWT token
-    path("refresh_token", jwt_token_refresh, name="token_obtain_pair"),
 ]
