c9ce62c8c51b04b0537d6902a8ce2666748e59d4
Radouane Iben-hamou <reben-ha@student.1337.ma>
2024-03-19 01:48:55 +0000

    use redirect from 'next/navigation'

--- Frontend/app/page.jsx
+++ Frontend/app/page.jsx
@@ -1,18 +1,5 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-	const router = useRouter();
-
-	useEffect(() => {
-		router.push("/home"); // Redirect to /home on component mount
-	}, [router]); // Dependency array to avoid infinite loops
-
-	return (
-		<div>
-			{/* You can optionally display some content while redirecting (optional) */}
-			Redirecting to Home page...
-		</div>
-	);
+	redirect("/home");
 }
