0d4af7c6599d7a0b9f4fd2ce79c63fdc7c520096
Radouane Iben-hamou <reben-ha@student.1337.ma>
2024-03-19 03:10:57 +0000

    #

--- Frontend/middleware.js
+++ Frontend/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 
 export default function middleware(request) {
-	const isAuthenticated = false; // TODO: check if Authenticated
+	const isAuthenticated = true; // TODO: check if Authenticated
 
 	if (!isAuthenticated)
 		return NextResponse.redirect(new URL("/login", request.url));
