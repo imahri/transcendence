2ebee9a1697209080c26c44727302ab2353eb51e
Redouane Iben Hamou <reben-ha@e3r1p5.1337.ma>
2024-03-22 22:31:56 +0000

    @RED-MEGA :  #

--- Frontend/app/(app)/chat/layout.jsx
+++ Frontend/app/(app)/chat/layout.jsx
@@ -1,11 +1,29 @@
 "use client";
 import { useEffect, useState } from "react";
 import ConversationSection from "./Components/Conversations/ConversationSection";
-import SideBar from "./Components/SideBar/SideBar";
 import Separator from "./Components/SideBar/Separator";
 import { wsChat } from "../../URLS";
 import { getToken } from "../../(auth)/AuthTools/tokenManagment";
-import { WsChatContext, ConvChatContext } from "./context/context";
+import {
+	WsChatContext,
+	ConvTypeChatContext,
+	ConvChatContext,
+} from "./context/context";
+import ConversationType from "./Components/SideBar/ConversationType";
+import Conversations from "./Components/SideBar/Conversations";
+import styles from "./styles/layout.module.css";
+import { Searchbar } from "@/app/(app)/searchBar/Searchbar";
+
+function SideBar({ children }) {
+	return (
+		<aside className={styles.sidebar}>
+			<Searchbar />
+			<Separator />
+			{children}
+			{/* <div className="bg-red-800 w-full h-[10%]"></div> */}
+		</aside>
+	);
+}
 
 function Separators() {
 	return (
@@ -16,9 +34,12 @@ function Separators() {
 	);
 }
 
+const conversation_types = ["Friends", "Groups"];
+
 export default function ChatLayout({ children }) {
 	const [socket, setSocket] = useState(null);
 	const [Conv, setConv] = useState(null);
+	const [convType, setConvType] = useState(conversation_types[0]);
 
 	useEffect(() => {
 		if (!socket) {
@@ -42,7 +63,14 @@ export default function ChatLayout({ children }) {
 		<WsChatContext.Provider value={socket}>
 			<ConvChatContext.Provider value={[Conv, setConv]}>
 				<div className="h-screen w-full m-0 flex flex-row bg-[#202020]">
-					<SideBar />
+					<SideBar>
+						<ConvTypeChatContext.Provider
+							value={[convType, setConvType]}
+						>
+							<ConversationType />
+							<Conversations type={convType} />
+						</ConvTypeChatContext.Provider>
+					</SideBar>
 					<Separators />
 					<ConversationSection />
 					{children}
