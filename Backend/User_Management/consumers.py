dc26c6545738b9731550fed71cebf0418600beea
oussama krich <skogar445@gmail.com>
2024-03-24 06:10:13 +0000

    switch WebsocketConsumer to AsyncJsonWebSocketConsumer

--- Backend/User_Management/consumers.py
+++ Backend/User_Management/consumers.py
@@ -1,13 +1,13 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.db import database_sync_to_async
 
 from .models import User
 
 
 
-class FriendShipConsumer(WebsocketConsumer):
+class FriendShipConsumer(AsyncJsonWebsocketConsumer):
     channels: dict = {}
 
     @classmethod
@@ -18,91 +18,93 @@ class FriendShipConsumer(WebsocketConsumer):
     def get_channel_by_user(cls, user: str):
         return cls.channels[user]
 
-    def connect(self):
+    async def connect(self):
         try:
            self.user: User = self.scope["user"]
            if self.user.is_anonymous:
                raise Exception("Not Authorizer")
            else:
             self.register_channel(self.user.username, self.channel_name)
-            self.accept()
+            await self.accept()
         
         except Exception as error:
            print('ws connect : ', error);
-           self.close();
+           await self.close();
 
 
     
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         username : str = self.user.username
         if username in self.channels:
             del self.channels[username]
 
     
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        action = text_data_json["action"]
-        friend_id = text_data_json["friend_id"]
-        friend : User = User.objects.get(pk=friend_id)
+    async def receive_json(self, text_data):
+        try:
+            action = text_data["action"]
+            friend_id = text_data["friend_id"]
+            friend : User = await database_sync_to_async(User.objects.get)(pk=friend_id)
 
 
-        if action == 'check':
-            self.check_friendship(friend)
-        else: 
-            self.set_friendship(friend, action)
+            if action == 'check':
+                await self.check_friendship(friend)
+            else: 
+                await self.set_friendship(friend, action)
+        
+        except Exception as error:
+            print('error : ', error)
 
 
-
-    def set_friendship(self, friend, action):
+    async def set_friendship(self, friend, action):
 
         if action == 'add':
             try:
-                self.user.add_friend(friend=friend)
+                await database_sync_to_async(self.user.add_friend)(friend=friend)
             except Exception as error:
                 print('error add: ', error)
         elif action == 'accept':
             try:
-                self.user.accept_friend(friend=friend)
+                await database_sync_to_async(self.user.accept_friend)(friend=friend)
             except Exception as error:
                 print('error accept: ', error)
 
         elif action == 'remove':
             try:
-                self.user.delete_friend(friend=friend)
+                await database_sync_to_async(self.user.delete_friend)(friend=friend)
             except Exception as error:
                 print('error remove :  ', error)
 
         elif action == 'block':
             try:
-                self.user.block_friend(friend=friend)
+                await database_sync_to_async(self.user.block_friend)(friend=friend)
             except Exception as error:
                 print('error block :  ', error)
         
         try:
             target_friend = self.get_channel_by_user(friend.username)
-            async_to_sync(self.channel_layer.send)(target_friend, {"type": "update"})
+            await self.channel_layer.send(target_friend, {"type": "update"})
         except Exception as error:
             print('channel key not found :', error)
 
-        self.check_friendship(friend)
+        await self.check_friendship(friend)
         
 
-    def update(self, event):
-        self.send(text_data=json.dumps({'status': 'update'}))
+    async def update(self, event):
+        await self.send_json(content={'status': 'update'})
         
 
 
-    def check_friendship(self ,friend):
+    async def check_friendship(self ,friend):
         
         if self.user.pk == friend.pk:
-            self.send(text_data=json.dumps({'status': 'owner'}))
+            await self.send_json(content={'status': 'owner'})
 
         else :
             try:
-                friendShip = self.user.get_friendship(friend=friend)
-                self.send(text_data=json.dumps({'status': friendShip.status}))
+                friendShip = await database_sync_to_async(self.user.get_friendship)(friend=friend)
+                await self.send_json(content={'status': friendShip.status})
                 
 
             except Exception:
-                self.send(text_data=json.dumps({'status': 'not friend'}))
+                await self.send_json(content={'status': 'not friend'})
             
