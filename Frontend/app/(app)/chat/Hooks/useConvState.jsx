9808c61c680a2918e7fcb932089db72dece5186a
Redouane Iben Hamou <reben-ha@e3r11p2.1337.ma>
2024-03-25 06:40:20 +0000

    @RED-MEGA :  set default type

--- Frontend/app/(app)/chat/Hooks/useConvState.jsx
+++ Frontend/app/(app)/chat/Hooks/useConvState.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export const useConvState = () => {
 	const [type, current_conv] = usePathname().split("/").slice(2, 4);
 	const [convState, setConvState] = useState({
-		type: type,
+		type: type === undefined ? "Friends" : type,
 		current_conv: current_conv === undefined ? null : current_conv,
 	});
 	return [convState, setConvState];
