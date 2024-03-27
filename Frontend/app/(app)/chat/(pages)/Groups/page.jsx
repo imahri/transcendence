463055d7c59e87be41196d9458805fea1cfc4839
Redouane Iben Hamou <reben-ha@e2r4p10.1337.ma>
2024-03-27 05:27:29 +0000

    @RED-MEGA :  Switch pages to pages/.

---
+++ Frontend/app/(app)/chat/(pages)/Groups/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { NoConv } from "../../Components/Conversations/NoConv";
+import { useContext } from "react";
+import { ConvChatContext } from "../../context/context";
+
+export default function Groups() {
+	return <NoConv />;
+}
--- Frontend/app/(app)/chat/Groups/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { NoConv } from "../Components/Conversations/NoConv";
-import { useContext } from "react";
-import { ConvChatContext } from "../context/context";
-
-export default function Groups() {
-	return <NoConv />;
-}
