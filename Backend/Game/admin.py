4d01bb4e9d59713e970db8165da7010d5f76b5c7
oussama krich <skogar445@gmail.com>
2024-03-28 01:51:24 +0000

    add some fields to admin panel

--- Backend/Game/admin.py
+++ Backend/Game/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Acheivement, Match, Grade, Items, Padel, Board, Badge
 # Register your models here.
+
+myModels = [Acheivement, Match, Grade, Items, Padel, Badge, Board]
+
+admin.site.register(myModels)
