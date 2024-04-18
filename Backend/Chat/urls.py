9d4a371af3e6b7e9bcc60ece89e584439b0b09f2
Redouane Iben Hamou <reben-ha@e2r5p10.1337.ma>
2024-04-18 16:43:08 +0100

    @RED-MEGA :  add GET conversations/<str:friend_name>

--- Backend/Chat/urls.py
+++ Backend/Chat/urls.py
@@ -3,5 +3,6 @@ from .views import ConversationView, MessageView
 
 urlpatterns = [
     path("conversations", ConversationView.as_view()),
+    path("conversations/<str:friend_name>", ConversationView.get_conversation),
     path("messages", MessageView.as_view()),
 ]
