2a89c6cfebcf5a4412527e9a4930295d04e5d3f7
Mahri Imad-Eddine <imahri@student.1337.ma>
2024-04-30 10:54:32 +0100

    game_play done front still some responce

--- Frontend/app/(app)/game_play/components/live_game/Livegame.jsx
+++ Frontend/app/(app)/game_play/components/live_game/Livegame.jsx
@@ -20,7 +20,24 @@ const LGame = () => {
 					<div className={styles.all_in}>
 						<div className={styles.bord_part}>
 							<div className={styles.borad}></div>
-							<div className={styles.score}></div>
+							<div className={styles.score}>
+								<div className={styles.score_first}></div>
+								<div className={styles.players}>
+									<div className={styles.fi_players}>
+										<div className={styles.pr_player}></div>
+										<div className={styles.sc_player}>
+											3
+										</div>
+									</div>
+									<span className={styles.versus}>VS</span>
+									<div className={styles.se_players}>
+										<div className={styles.pr_player}></div>
+										<div className={styles.sc_player}>
+											3
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 
 						<div className={styles.mode_part}>
