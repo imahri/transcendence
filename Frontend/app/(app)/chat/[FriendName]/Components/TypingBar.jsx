3e069a8c245a757966aa893c7e6b0fa0596e23f3
Redouane Iben Hamou <reben-ha@e2r5p10.1337.ma>
2024-04-30 15:42:18 +0100

    @RED-MEGA :  set limit to TypingBar, 300 <=

--- Frontend/app/(app)/chat/[FriendName]/Components/TypingBar.jsx
+++ Frontend/app/(app)/chat/[FriendName]/Components/TypingBar.jsx
@@ -4,17 +4,27 @@ import styles from "./styles/TypingBar.module.css";
 
 export function TypingBar({ onSend }) {
 	const inputMessage = useRef();
+	const labelRef = useRef();
 
 	const handleClick = () => {
 		if (inputMessage.current.value.trim().length === 0) return;
 		onSend(inputMessage.current.value);
 		inputMessage.current.value = "";
+		labelRef.current.classList.remove(styles.reachTheLimit);
 	};
+
+	const handleChange = () =>
+		labelRef.current.classList.toggle(
+			styles.reachTheLimit,
+			inputMessage.current.value.length >= 300,
+		);
 
 	return (
 		<div className={styles.container}>
-			<label>
+			<label ref={labelRef}>
 				<input
+					onChange={handleChange}
+					maxLength={300}
 					onKeyDown={(e) => e.key === "Enter" && handleClick()}
 					ref={inputMessage}
 					type="text"
