1a8da735c9efe2d45d3d868d67313ec5be1832e3
Mahri Imad-Eddine <imahri@student.1337.ma>
2024-05-05 16:27:29 +0100

    ball in backend

--- Backend/Game/consumers.py
+++ Backend/Game/consumers.py
@@ -1,3 +1,6 @@
+from cgitb import text
+from email import message
+from sys import stderr
 from typing import Self
 import math
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
@@ -5,129 +8,165 @@ from channels.db import database_sync_to_async
 from User_Management.models import User
 import json
 import time
-
+import sys
+from asgiref.sync import async_to_sync
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 # create class game
 # construct
 
 # ws://localhost:8000/ws/game
 
+
+
+
 class Game:
     def __init__(self):
-        self.downKeyPressed = False
-        self.upKeyPressed = False
+        # self.downKeyPressed = False
+        # self.upKeyPressed = False
   
         self.canvas = {
-            "width":300,
-            "height":500
+            "width":1000,
+            "height":900
         }
+        # self.canvas = {
+        #     "width": canvas_width,
+        #     "height": canvas_height
+        # }
+
+        # self.user = {
+        #     "x": 3,
+        #     "y": self.canvas['height'] / 2 - 200 / 2,
+        #     "width": 25,
+        #     "height": 200,
+        #     "color": "orange",
+        #     "score": 0
+        # }
+        # self.user1 = {
+        #     "x": 3,
+        #     "y": self.canvas['height'] / 2 - 200 / 2,
+        #     "width": 25,
+        #     "height": 200,
+        #     "color": "orange",
+        #     "score": 0
+        # }
+        # self.com = {
+        #     "x": self.canvas['width'] - 28,
+        #     "y": self.canvas['height'] / 2 - 200 / 2,
+        #     "width": 25,
+        #     "height": 200,
+        #     "color": "orange",
+        #     "score": 0
+        # }
         
-        self.user = {
-            "x": 3,
-            "y": self.canvas['height'] / 2 - 200 / 2,
-            "width": 25,
-            "height": 200,
-            "color": "orange",
-            "score": 0
-        }
-        self.user1 = {
-            "x": 3,
-            "y": self.canvas['height'] / 2 - 200 / 2,
-            "width": 25,
-            "height": 200,
-            "color": "orange",
-            "score": 0
-        }
-        self.com = {
-            "x": self.canvas['width'] - 28,
-            "y": self.canvas['height'] / 2 - 200 / 2,
-            "width": 25,
-            "height": 200,
-            "color": "orange",
-            "score": 0
-        }
         self.ball = {
             "x": self.canvas['width'] / 2,
             "y": self.canvas['height'] / 2,
-            "radius": 20,
+            "radius": 30,
             "speed": 10,
             "velocityX": 5,
             "velocityY": 5,
             "color": "orange"
         }
         
+    # def receive(self, data):
+    #     print(data)
+        # data_json = json.load(data)
+        # resizeh = data_json.get("canvasWidth")
+        # resizew = data_json.get("canvasHeight")
+        # print("h  " + str(resizeh))
+        # print("w  " + str(resizew))
+        # print("w  XX")
+
+    # def reset_ball(self):
+    #     self.ball['x'] = self.canvas['width'] / 2
+    #     self.ball['y'] = self.canvas['height'] / 2
+    #     self.ball['speed'] = 10
+    #     self.ball['velocityX'] *= -1
         
 
-    def reset_ball(self):
-        self.ball['x'] = self.canvas['width'] / 2
-        self.ball['y'] = self.canvas['height'] / 2
-        self.ball['speed'] = 10
-        self.ball['velocityX'] *= -1
-        
-
-    def updatePaddlePosition(self):
-        if (self.upKeyPressed and self.user['y'] > 0):
-            self.user['y'] -= 10
-        if (self.downKeyPressed and self.user['y'] + self.user['height'] < self.canvas['height']):
-            self.user['y'] += 10
+    # def updatePaddlePosition(self):
+    #     if (self.upKeyPressed and self.user['y'] > 0):
+    #         self.user['y'] -= 10
+    #     if (self.downKeyPressed and self.user['y'] + self.user['height'] < self.canvas['height']):
+    #         self.user['y'] += 10
 
 
-    def collision(self,ball, user):
-        bat = self.ball['y'] - self.ball['radius']
-        bab = self.ball['y'] + self.ball['radius']
-        bal = self.ball['x'] - self.ball['radius']
-        bar = self.ball['x'] + self.ball['radius']
+    # def collision(self,ball, user):
+    #     bat = self.ball['y'] - self.ball['radius']
+    #     bab = self.ball['y'] + self.ball['radius']
+    #     bal = self.ball['x'] - self.ball['radius']
+    #     bar = self.ball['x'] + self.ball['radius']
         
-        ut= user['y']
-        ub = user['y'] + user['height']
-        ul = user['x']
-        ur = user['x'] + user['width']
+    #     ut= user['y']
+    #     ub = user['y'] + user['height']
+    #     ul = user['x']
+    #     ur = user['x'] + user['width']
         
-        return (bat > ul and bab > ut and bal < ur and bat < ub)
+    #     return (bat > ul and bab > ut and bal < ur and bat < ub)
     
-    
+
     def update_ball(self):
-        print("allo")
         self.ball['x'] += self.ball['velocityX']
         self.ball['y'] += self.ball['velocityY']
-        print("self.ball['x'] >> " + str(self.ball['x']))
-        print("self.ball['y'] >> " + str(self.ball['y']))
-
         
-        computerLevl = 0.5
-        
-        self.com['y'] += (self.ball['y'] - (self.com['y'] + self.com['height'] / 2)) * computerLevl
-        
-        if (self.ball['y'] + self.ball['radius'] > self.canvas['height'] or self.ball['y'] - self.ball['radius'] < 0 ):
+        if (self.ball['y'] + self.ball['radius'] > self.canvas['height'] or self.ball['y'] - self.ball['radius'] < 0):
             self.ball['velocityY'] *= -1
+        if (self.ball['x'] - self.ball['radius'] < 0 or self.ball['x'] + self.ball['radius'] > self.canvas['width']):
+            self.ball['velocityX'] *= -1
+        
+        
+        # print("self.ball['x'] >> " + str(self.ball['x']))
+        # print("self.ball['y'] >> " + str(self.ball['y']))
+
             
-        if self.ball['x'] < self.canvas['width'] / 2:
-            player = self.user
-        else:
-            player = self.com
-        if (self.collision(self.ball,player)):
+        
+   
+        # print("allo")
+        # print("self.user['x'] >> " + str(self.user['x']))
+        # print("self.user['y'] >> " + str(self.user['y']))
+        # print("self.com['x'] >> " + str(self.com['x']))
+        # print("self.com['y'] >> " + str(self.com['y']))
+
+        
+        # computerLevl = 0.5
+        
+        # self.com['y'] += (self.ball['y'] - (self.com['y'] + self.com['height'] / 2)) * computerLevl
+        
+        # if (self.ball['y'] + self.ball['radius'] > self.canvas['height'] or self.ball['y'] - self.ball['radius'] < 0 ):
+        #     self.ball['velocityY'] *= -1
             
-            collpoint = self.ball['y'] - (player['y'] + player['height'] / 2)
+        # if self.ball['x'] < self.canvas['width'] / 2:
+        #     player = self.user
+        # else:
+        #     player = self.com
+        
+        # if (self.collision(self.ball,player)):
             
-            collpoint = collpoint / (player['height'] / 2)
+        #     collpoint = self.ball['y'] - (player['y'] + player['height'] / 2)
             
-            angleRad = (collpoint * math.pi) / 4
+        #     collpoint = collpoint / (player['height'] / 2)
             
-            direction = 1 if self.ball['x'] < self.canvas['width'] / 2 else -1
+        #     angleRad = (collpoint * math.pi) / 4
             
-            self.ball['velocityX'] = direction * self.ball['speed'] * math.cos(angleRad)
+        #     direction = 1 if self.ball['x'] < self.canvas['width'] / 2 else -1
             
-            self.ball['velocityY'] = self.ball['speed'] * math.sin(angleRad)
+        #     self.ball['velocityX'] = direction * self.ball['speed'] * math.cos(angleRad)
             
-            self.ball['speed'] += 0.5
+        #     self.ball['velocityY'] = self.ball['speed'] * math.sin(angleRad)
             
-        if ((self.ball['x'] - self.ball['radius']) < 0):
-            self.com['score'] += 1
-            self.reset_ball()
-            print("com +1 >> " + str(self.com['score']))
-        elif ((self.ball['x'] + self.ball['radius']) > self.canvas['width']):
-            self.user['score'] += 1
-            self.reset_ball()
-            print("user +1 >> " + str(self.user['score']))
+        #     self.ball['speed'] += 0.5
+
+
+
+        # if ((self.ball['x'] - self.ball['radius']) < 0):
+        #     self.com['score'] += 1
+        #     self.reset_ball()
+        #     print("com +1 >> " + str(self.com['score']))
+        # elif ((self.ball['x'] + self.ball['radius']) > self.canvas['width']):
+        #     self.user['score'] += 1
+        #     self.reset_ball()
+        #     print("user +1 >> " + str(self.user['score']))
                 
                 
 
@@ -138,35 +177,92 @@ class Game:
         
             
         
-
-            
-            
-            
-            
-            
-
-
 
 class GameConsumer(AsyncJsonWebsocketConsumer):
     
+    
     async def connect(self):
-        # try:
-            # self.user: User = self.scope["user"]
-            # print(self.user)
-            # if self.user.is_anonymous:
-            #     raise Exception("Not Authorizer")
-            # else:
-            #     self.register_channel(self.user.username, self.channel_name)
         await self.accept()
         await self.send_json(content={"type": "Connected"})
-        loba = Game()
+        self.loba = Game()
+        await asyncio.sleep(1)
+        self.game_task = asyncio.create_task(self.send_ball_coordinates())
+    
+    # async def connect(self):
+    #     await self.accept()
+    #     await self.send_json(content={"type": "Connected"})
+    #     canvas_width = 1000  # Default width
+    #     canvas_height = 900  # Default height
+    #     self.loba = Game(canvas_width, canvas_height)
+    #     self.game_task = asyncio.create_task(self.send_ball_coordinates())
+
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        self.loba.canvas['height'] = data.get("canvasWidth")
+        self.loba.canvas['width'] = data.get("canvasHeight")
+
+    # async def receive(self, text_data):
+    #     data = json.loads(text_data)
+    #     canvas_height = data.get("canvasWidth")
+    #     canvas_width = data.get("canvasHeight")
+    #     self.loba.canvas['height'] = canvas_height
+    #     self.loba.canvas['width'] = canvas_width
+
+    async def send_ball_coordinates(self):
+        
         while True:
-            loba.update_ball()
-            loba.updatePaddlePosition()
-            time.sleep(0.4)
-            
-       # except Exception:
-        #     await self.close()
-            
-            
-            
+            self.loba.update_ball()
+            x = self.loba.ball['x']
+            y = self.loba.ball['y']
+            await asyncio.sleep(0.0075)
+            await self.send_json(content={
+                    'event': 'update',
+                    'x': y,
+                    'y': x
+                })
+        
+
+
+        # print(self.loba.canvas['height'])
+        # print(self.loba.canvas['width'])
+        # self.loba.receive(text_data)
+        # self.send_ball_coordinates(text_data)
+
+
+# class GameConsumer(AsyncJsonWebsocketConsumer):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.loba = Game()
+#         self.send_ball_updates = False  # Flag to control sending ball updates
+
+#     async def connect(self):
+#         await self.accept()
+#         await self.send_json(content={"type": "Connected"})
+#         self.send_ball_updates = True  # Set the flag to True
+
+
+#     async def receive(self, text_data):
+#         try:
+#             data = json.loads(text_data)
+#             if "canvasWidth" in data and "canvasHeight" in data:
+#                 self.loba.canvas['width'] = data.get("canvasHeight")
+#                 self.loba.canvas['height'] = data.get("canvasWidth")
+#                 await self.send_ball_coordinates()
+#         except json.JSONDecodeError:
+#             pass
+
+#     async def send_ball_coordinates(self):
+#         while self.send_ball_updates:
+#             self.loba.update_ball()
+#             x = self.loba.ball['x']
+#             y = self.loba.ball['y']
+#             await self.send_json(content={
+#                 'event': 'update',
+#                 'x': y,
+#                 'y': x
+#             })
+#             await asyncio.sleep(0.0075)
+
+#     async def disconnect(self, close_code):
+#         self.send_ball_updates = False
--- Frontend/app/(app)/game_play/components/live_game/game.jsx
+++ Frontend/app/(app)/game_play/components/live_game/game.jsx
@@ -1,214 +1,110 @@
 "use client";
 import Script from "next/script";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./LGame.module.css";
 
+let xcord = 0;
+let ycord = 0;
 export const Youchen = () => {
 	const cvs = useRef(null);
+	const [socket, setSocket] = useState(null);
+	console.log(111111);
 
 	useEffect(() => {
-		const canvas = cvs.current;
-		const ctx = canvas.getContext("2d");
-
-		canvas.width = document.documentElement.clientWidth;
-		canvas.height = document.documentElement.clientHeight;
-
-		// draw rectangle
-
-		function drawRect(x, y, w, h, color) {
-			ctx.fillStyle = color;
-			ctx.fillRect(x, y, w, h);
-		}
-
-		// draw rectangle
-
-		function drawCircle(x, y, r, color) {
-			ctx.fillStyle = color;
-			ctx.beginPath();
-			ctx.arc(x, y, r, 0, Math.PI * 2, false);
-			ctx.closePath();
-			ctx.fill();
-		}
-
-		const user = {
-			x: 3,
-			y: canvas.height / 2 - 200 / 2,
-			// y: 0,
-			width: 25,
-			height: 200,
-			color: "orange",
-			score: 0,
+		const ws = new WebSocket("ws://localhost:8000/ws/game");
+		ws.onopen = () => {
+			console.log("opened");
+			setSocket(ws);
 		};
 
-		const com = {
-			x: canvas.width - 28,
-			y: canvas.height / 2 - 200 / 2,
-			width: 25,
-			height: 200,
-			color: "orange",
-			score: 0,
+		ws.onmessage = (event) => {
+			const data = JSON.parse(event.data);
+			if (data.event === "update") {
+				xcord = data.x;
+				ycord = data.y;
+			}
 		};
 
-		const ball = {
-			x: canvas.width / 2,
-			y: canvas.height / 2,
-			radius: 20,
-			speed: 20,
-			velocityX: 5,
-			velocityY: 5,
-			color: "orange",
+		ws.onerror = () => {
+			console.log("error happened");
 		};
-
-		// mouves
-
-		let upKeyPressed = false;
-		let downKeyPressed = false;
-
-		document.addEventListener("keydown", keyDownHandler);
-		document.addEventListener("keyup", keyUpHandler);
-
-		function keyDownHandler(event) {
-			console.log(event.keyCode);
-			if (event.keyCode === 38) {
-				upKeyPressed = true;
-				// ws.current.send(JSON.stringify({ message }));
-			}
-			if (event.keyCode === 87) {
-				upKeyPressed = true;
-			}
-			if (event.keyCode === 40) {
-				downKeyPressed = true;
-			}
-			if (event.keyCode === 83) {
-				downKeyPressed = true;
-			}
-		}
-
-		function keyUpHandler(event) {
-			if (event.keyCode === 38) {
-				upKeyPressed = false;
-			}
-			if (event.keyCode === 87) {
-				upKeyPressed = false;
-			}
-			if (event.keyCode === 40) {
-				downKeyPressed = false;
-			}
-			if (event.keyCode === 83) {
-				downKeyPressed = false;
-			}
-		}
-
-		function updatePaddlePosition() {
-			if (upKeyPressed && user.y > 0) {
-				user.y -= 10;
-			}
-			if (downKeyPressed && user.y + user.height < canvas.height) {
-				user.y += 10;
-			}
-		}
-
-		function drawText(text, x, y, color) {
-			ctx.fillStyle = color;
-			ctx.font = "45px fantasy";
-			ctx.fillText(text, x, y);
-		}
-
-		function movePaddle(evt) {
-			let rect = canvas.getBoundingClientRect();
-			user.y = evt.clientY - rect.top - user.height / 2;
-		}
-
-		function collision(b, p) {
-			b.top = b.y - b.radius;
-			b.bottom = b.y + b.radius;
-			b.left = b.x - b.radius;
-			b.right = b.x + b.radius;
-
-			p.top = p.y;
-			p.bottom = p.y + p.height;
-			p.left = p.x;
-			p.right = p.x + p.width;
-
-			return (
-				b.right > p.left &&
-				b.bottom > p.top &&
-				b.left < p.right &&
-				b.top < p.bottom
-			);
-		}
-
-		function render() {
-			drawText(user.score, canvas.width / 4, canvas.height / 5, "white");
-			drawText(
-				com.score,
-				(3 * canvas.width) / 4,
-				canvas.height / 5,
-				"white",
-			);
-
-			drawRect(user.x, user.y, user.width, user.height, user.color);
-			drawRect(com.x, com.y, com.width, com.height, com.color);
-
-			drawCircle(ball.x, ball.y, ball.radius, ball.color);
-		}
-
-		function resetBall() {
-			ball.x = canvas.width / 2;
-			ball.y = canvas.height / 2;
-
-			ball.speed = 20;
-			ball.velocityX *= -1;
-		}
-
-		function update() {
-			ball.x += ball.velocityX;
-			ball.y += ball.velocityY;
-
-			let computerLevel = 0.5;
-			com.y += (ball.y - (com.y + com.height / 2)) * computerLevel;
-
-			if (
-				ball.y + ball.radius > canvas.height ||
-				ball.y - ball.radius < 0
-			) {
-				ball.velocityY *= -1;
-			}
-
-			let player = ball.x < canvas.width / 2 ? user : com;
-
-			if (collision(ball, player)) {
-				let collpoint = ball.y - (player.y + player.height / 2);
-
-				collpoint = collpoint / (player.height / 2);
-
-				let angleRad = (collpoint * Math.PI) / 4;
-
-				let direction = ball.x < canvas.width / 2 ? 1 : -1;
-
-				ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-				ball.velocityY = ball.speed * Math.sin(angleRad);
-				ball.speed += 0.5;
-			}
-			if (ball.x - ball.radius < 0) {
-				com.score++;
-				resetBall();
-			} else if (ball.x + ball.radius > canvas.width) {
-				user.score++;
-				resetBall();
-			}
-		}
-
-		function game() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			window.requestAnimationFrame(game);
-			render();
-			update();
-			updatePaddlePosition();
-		}
-
-		game();
+		ws.onclose = () => {
+			console.log("closed");
+		};
 	}, []);
+
+	useEffect(() => {
+		if (socket && socket.readyState === WebSocket.OPEN) {
+			const canvas = cvs.current;
+			const ctx = canvas.getContext("2d");
+
+			canvas.width = document.documentElement.clientWidth;
+			canvas.height = document.documentElement.clientHeight;
+
+			const ball = {
+				x: canvas.width / 2,
+				y: canvas.height / 2,
+				radius: 30,
+				speed: 20,
+				velocityX: 5,
+				velocityY: 5,
+				color: "orange",
+			};
+
+			function drawRect(x, y, w, h, color) {
+				ctx.fillStyle = color;
+				ctx.fillRect(x, y, w, h);
+			}
+
+			function drawCircle(x, y, r, color) {
+				ctx.fillStyle = color;
+				ctx.beginPath();
+				ctx.arc(x, y, r, 0, Math.PI * 2, false);
+				ctx.closePath();
+				ctx.fill();
+			}
+
+			function render() {
+				drawCircle(xcord, ycord, 20, "orange");
+				drawRect(0, canvas.height / 2 - 200 / 2, 30, 200, "orange");
+				drawRect(
+					canvas.width - 30,
+					canvas.height / 2 - 200 / 2,
+					30,
+					200,
+					"orange",
+				);
+			}
+
+			function game() {
+				canvas.width = document.documentElement.clientWidth;
+				canvas.height = document.documentElement.clientHeight;
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				window.requestAnimationFrame(game);
+				render();
+			}
+
+			game();
+
+			setInterval(() => {
+				if (socket && socket.readyState === WebSocket.OPEN) {
+					console.log(canvas.height);
+					console.log(canvas.width);
+					socket.send(
+						JSON.stringify({
+							event: "resize",
+							canvasHeight: canvas.height,
+							canvasWidth: canvas.width,
+						}),
+					);
+				}
+			}, 1000);
+
+			return () => {
+				console.log("getting out");
+			};
+		}
+	}, [socket]);
 	return (
 		<canvas style={{ height: "100%", width: "100%" }} ref={cvs}></canvas>
 	);
