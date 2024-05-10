6970d83ae77d6e5ad0c00b4da8b7a0aac28e0fe9
Redouane Iben Hamou <reben-ha@e2r4p7.1337.ma>
2024-05-10 16:40:06 +0100

    @RED-MEGA :  #

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pipenv shell
-
 django-admin makemigrations
 
 django-admin migrate
 
-django-admin runserver 0.0.0.0:4242
+openssl req -x509 -newkey rsa:4096 -keyout transcendence.com.key -out transcendence.com.crt -days 365 -subj "/C=MA/ST=BNK/L=KH/O=1337/CN=transcendence.com"
+
+daphne -b 0.0.0.0:8080 --ssl --cert-file transcendence.com.crt --key-file transcendence.com.key core.asgi:application
