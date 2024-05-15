0466ac050ed988c589e097917378de1acc12c312
Redouane Iben Hamou <reben-ha@e3r1p4.1337.ma>
2024-05-15 20:32:30 +0100

    @RED-MEGA :  #

--- Frontend/app/(app)/chat/Components/Conversations.jsx
+++ Frontend/app/(app)/chat/Components/Conversations.jsx
@@ -12,65 +12,57 @@ import { Iceland } from "next/font/google";
 
 const iceland = Iceland({ weight: "400", subsets: ["latin"] });
 
-function Unseen({ count }) {
-	return (
-		<div
-			className="
+const Unseen = ({ count }) => (
+	<div
+		className="
             h-6 w-6
 			mb-3 pb-0.3
             bg-gradient-to-r from-[#7D46F2] to-[#CB3737]
             flex justify-center items-center
             rounded-full
             "
-		>
-			<small className="cursor-default text-white">{count}</small>
-		</div>
-	);
-}
+	>
+		<small className="cursor-default text-white">{count}</small>
+	</div>
+);
 
-function ProfileImage({ src }) {
-	return (
-		<div className={styles.image}>
-			<Image
-				width={100}
-				height={100}
-				className={styles.img}
-				src={src}
-				alt="Profile"
-			/>
-		</div>
-	);
-}
+const ProfileImage = ({ src }) => (
+	<div className={styles.image}>
+		<Image
+			width={100}
+			height={100}
+			className={styles.img}
+			src={src}
+			alt="Profile"
+		/>
+	</div>
+);
 
-function FriendInfo({ friend_name, last_msg }) {
-	return (
-		<div className="h-full flex-grow flex flex-col justify-start">
-			<h3 className="pt-4 text-start text-xl font-semibold text-white">
-				{friend_name}
-			</h3>
-			{last_msg && (
-				<small className="mt-2 text-start font-medium text-[#7D7D7D]">
-					{last_msg.length > 20
-						? `${last_msg.substring(0, 20)}...`
-						: last_msg}
-				</small>
-			)}
-		</div>
-	);
-}
-
-function TimeNotification({ time, unseen_msg }) {
-	return (
-		<div className="h-full w-[18%] rounded-r-sm flex justify-center items-center flex-col">
-			<small className="mr-2 mt-2 font-light text-xs text-[#7D7D7D]">
-				{ToHour12Time(time)}
+const FriendInfo = ({ friend_name, last_msg }) => (
+	<div className="h-full flex-grow flex flex-col justify-start">
+		<h3 className="pt-4 text-start text-xl font-semibold text-white">
+			{friend_name}
+		</h3>
+		{last_msg && (
+			<small className="mt-2 text-start font-medium text-[#7D7D7D]">
+				{last_msg.length > 20
+					? `${last_msg.substring(0, 20)}...`
+					: last_msg}
 			</small>
-			<div className="flex-grow w-full flex justify-center items-center">
-				{unseen_msg > 0 && <Unseen count={unseen_msg} />}
-			</div>
+		)}
+	</div>
+);
+
+const TimeNotification = ({ time, unseen_msg }) => (
+	<div className="h-full w-[18%] rounded-r-sm flex justify-center items-center flex-col">
+		<small className="mr-2 mt-2 font-light text-xs text-[#7D7D7D]">
+			{ToHour12Time(time)}
+		</small>
+		<div className="flex-grow w-full flex justify-center items-center">
+			{unseen_msg > 0 && <Unseen count={unseen_msg} />}
 		</div>
-	);
-}
+	</div>
+);
 
 function Conversation({
 	user,
