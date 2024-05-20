41c406cd4e7fc2befea4eaf66a9dcd98c8287708
Redouane Iben Hamou <reben-ha@e2r5p6.1337.ma>
2024-05-20 15:55:20 +0100

    @RED-MEGA :  add create, join methods in Tournament Model

--- Backend/Tournament/models.py
+++ Backend/Tournament/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from User_Management.models import User
+
 
 class Tournament(models.Model):
 
@@ -7,8 +9,20 @@ class Tournament(models.Model):
         "Tournament.Participant", related_name="tournaments"
     )
     schedule = models.JSONField()
-    isEnd = models.BooleanField()
+    isEnd = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
+
+    @staticmethod
+    def create(name: str, creator: User, creator_unique_name: str):
+        tournament = Tournament(name=name)
+        tournament.save()
+        tournament.participants.add(
+            Participant.objects.create(name=creator_unique_name, user=creator)
+        )
+        return tournament
+
+    def join(self, user: User, unique_name: str):
+        self.participants.add(Participant.objects.create(name=unique_name, user=user))
 
 
 class Participant(models.Model):
