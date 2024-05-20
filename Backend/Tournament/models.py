36f36867a7b9b8e220642aa912808b13dd98d0b1
Redouane Iben Hamou <reben-ha@e2r5p6.1337.ma>
2024-05-20 14:48:49 +0100

    @RED-MEGA :  add Tournament model

--- Backend/Tournament/models.py
+++ Backend/Tournament/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Tournament(models.Model):
+
+    name = models.CharField(max_length=20, unique=True)
+    participants = models.ManyToManyField(
+        "Tournament.Participant", related_name="tournaments"
+    )
+    schedule = models.JSONField()
+    isEnd = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Participant(models.Model):
+
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey("User_Management.Model", on_delete=models.CASCADE)
