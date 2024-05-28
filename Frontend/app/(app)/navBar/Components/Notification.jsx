aaf80d8e3b306f11aa127df36d1fdbfdee611540
oussama krich <skogar445@gmail.com>
2024-05-28 13:48:13 +0100

    split accept decline buttons

--- Frontend/app/(app)/navBar/Components/Notification.jsx
+++ Frontend/app/(app)/navBar/Components/Notification.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { ChatSvg, GmaeSvg, FriendSvg } from "./AllSvg";
+import { ChatSvg, GmaeSvg, FriendSvg, AccepSvg, DeclineSvg } from "./AllSvg";
 import { UserContext } from "../../context";
 import { fetch_jwt } from "@/Tools/fetch_jwt_client";
 import { IMAGE_URL, NOTIF_URL } from "@/app/URLS";
@@ -15,45 +15,24 @@ import {
 	setType,
 } from "./NotifUtils";
 
+function acceptDecline(callBack, svg) {
+	return (
+		<button
+			onClick={callBack}
+			className="size-[20px] rounded-full cursor-pointer flex justify-center items-center"
+		>
+			{svg}
+		</button>
+	);
+}
+
 function AcceptDeclineGame({ notif, ws }) {
 	//send friend id to endpoint and send notif to user inform it friend accept your invit
 	// if decline send notif to other user user decline
 	return (
 		<div className="flex justify-between items-center gap-[5px]">
-			<button
-				onClick={() => accept(notif)}
-				className="size-[20px] rounded-full cursor-pointer flex justify-center items-center"
-			>
-				<svg
-					width="18"
-					height="18"
-					viewBox="0 0 108 81"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M36.7442 79.0306L1.87814 44.1646C-0.216542 42.0699 -0.216542 38.6736 1.87814 36.5787L9.46381 28.9928C11.5585 26.8979 14.955 26.8979 17.0497 28.9928L40.5371 52.48L90.8445 2.17282C92.9392 0.0781353 96.3357 0.0781353 98.4304 2.17282L106.016 9.7587C108.111 11.8534 108.111 15.2497 106.016 17.3446L44.33 79.0308C42.2351 81.1255 38.8389 81.1255 36.7442 79.0306Z"
-						fill="#7D7D7D"
-					/>
-				</svg>
-			</button>
-			<button
-				onClick={() => decline(ws, notif)}
-				className="size-[20px] rounded-full cursor-pointer flex justify-center items-center"
-			>
-				<svg
-					width="15"
-					height="15"
-					viewBox="0 0 80 80"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M55.1636 40L77.9068 17.2568C80.6977 14.4659 80.6977 9.94091 77.9068 7.14773L72.8523 2.09318C70.0614 -0.697727 65.5364 -0.697727 62.7432 2.09318L40 24.8364L17.2568 2.09318C14.4659 -0.697727 9.94091 -0.697727 7.14773 2.09318L2.09318 7.14773C-0.697727 9.93864 -0.697727 14.4636 2.09318 17.2568L24.8364 40L2.09318 62.7432C-0.697727 65.5341 -0.697727 70.0591 2.09318 72.8523L7.14773 77.9068C9.93864 80.6977 14.4659 80.6977 17.2568 77.9068L40 55.1636L62.7432 77.9068C65.5341 80.6977 70.0614 80.6977 72.8523 77.9068L77.9068 72.8523C80.6977 70.0614 80.6977 65.5364 77.9068 62.7432L55.1636 40Z"
-						fill="#7D7D7D"
-					/>
-				</svg>
-			</button>
+			{acceptDecline(() => accept(notif), AccepSvg)}
+			{acceptDecline(() => decline(ws, notif), DeclineSvg)}
 		</div>
 	);
 }
@@ -95,7 +74,7 @@ function NotifSection({ notif, ws }) {
 				<h2 className="text-[#7D7D7D] text-[10px]">{time}</h2>
 			</div>
 			<div
-				className={`absolute size-[10px] bg-greatBlue rounded-full right-[5px] ${notif.is_read || notif.content?.type == "invit" ? "hidden" : ""}`}
+				className={`absolute size-[10px] bg-greatBlue rounded-full right-[15px] ${notif.is_read || notif.content?.type == "invit" ? "hidden" : ""}`}
 			></div>
 		</Link>
 	);
