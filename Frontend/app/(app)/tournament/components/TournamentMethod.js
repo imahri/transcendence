769679825c54c7c8c6d3b8bfa7854f568e6dd3b3
oussama krich <skogar445@gmail.com>
2024-05-31 16:15:11 +0100

    fix tournament buttons and deblock to unblock

--- Frontend/app/(app)/tournament/components/TournamentMethod.js
+++ Frontend/app/(app)/tournament/components/TournamentMethod.js
@@ -2,7 +2,15 @@ import { fetch_jwt } from "@/Tools/fetch_jwt_client";
 import { STARTOUR_URL, TOURNAMENT_URL } from "@/app/URLS";
 import { myseterror } from "./CreateTournament";
 
-export async function joinTournament(id, user, nickname, setError, setDemo) {
+export async function joinTournament(
+	id,
+	user,
+	nickname,
+	setError,
+	setDemo,
+	setInfo,
+	setButton,
+) {
 	//PUT add user to a tournament
 	if (!nickname) nickname = user.username;
 	const body = JSON.stringify({ tournament_id: id, alias_name: nickname });
@@ -23,11 +31,11 @@ export async function joinTournament(id, user, nickname, setError, setDemo) {
 		return;
 	}
 	setDemo(data);
+	myseterror(setInfo, "User Joined the Tournament");
+	setButton("Quit");
 }
 
-export async function startTournament(id, setError) {
-	//start tournament
-
+export async function startTournament(id, setError, setButton, setInfo) {
 	const [isOk, status, data] = await fetch_jwt(
 		STARTOUR_URL,
 		{ id: id },
@@ -39,6 +47,8 @@ export async function startTournament(id, setError) {
 		myseterror(setError, data);
 		return;
 	}
+	myseterror(setInfo, "Tournament is Stareted");
+	setButton("Stareted");
 	console.log(data);
 }
 
