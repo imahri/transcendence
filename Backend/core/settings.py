799e303e3fb295acb3a3d1b25935af7e19e6f964
oussama krich <skogar445@gmail.com>
2024-06-09 12:07:09 +0100

    add chennels dependency and add redis to network

--- Backend/core/settings.py
+++ Backend/core/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders",
+    "channels",
     "User_Management",
     "Authentication",
     "Game",
