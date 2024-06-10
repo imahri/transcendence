fc85059dceb5a7c872ac005374aadf0768673728
oussama krich <skogar445@gmail.com>
2024-06-10 11:09:22 +0100

    -nodes

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -23,8 +23,8 @@ while ! pg_isready  -h database -U $POSTGRES_USER  -d $POSTGRES_DB  2> /dev/null
 
 ./manage.py init_acheivement
 
-openssl req -x509 -newkey rsa:4096 -keyout transcendence.com.key -out transcendence.com.crt -days 365 -subj "/C=MA/ST=BNK/L=KH/O=1337/CN=transcendence.com"
+openssl req -x509 -newkey rsa:4096 -keyout transcendence.com.key -out transcendence.com.crt -days 365 -subj "/C=MA/ST=BNK/L=KH/O=1337/CN=transcendence.com" -nodes
 
-# daphne -b 0.0.0.0 -p 8080 -e ssl:443:privateKey=./transcendence.com.key:certKey=./transcendence.com.crt core.asgi:application
+daphne -b 0.0.0.0 -p 8000 -e ssl:443:privateKey=./transcendence.com.key:certKey=./transcendence.com.crt core.asgi:application
 
-daphne -b 0.0.0.0 -p 8000 core.asgi:application
+# daphne -b 0.0.0.0 -p 8000 core.asgi:application
