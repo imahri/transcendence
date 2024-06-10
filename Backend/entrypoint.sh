482d08363922890e572c50c604fc674ae96dd20f
oussama krich <skogar445@gmail.com>
2024-06-10 11:30:15 +0100

    add https

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -25,6 +25,4 @@ while ! pg_isready  -h database -U $POSTGRES_USER  -d $POSTGRES_DB  2> /dev/null
 
 openssl req -x509 -newkey rsa:4096 -keyout transcendence.com.key -out transcendence.com.crt -days 365 -subj "/C=MA/ST=BNK/L=KH/O=1337/CN=transcendence.com" -nodes
 
-daphne -b 0.0.0.0 -p 8000 -e ssl:443:privateKey=./transcendence.com.key:certKey=./transcendence.com.crt core.asgi:application
-
-# daphne -b 0.0.0.0 -p 8000 core.asgi:application
+daphne -b 0.0.0.0 -e ssl:443:privateKey=./transcendence.com.key:certKey=./transcendence.com.crt core.asgi:application
